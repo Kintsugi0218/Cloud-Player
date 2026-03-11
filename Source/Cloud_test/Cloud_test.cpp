92126f8700b0dbd4ccde13e8fcf3bfaab4a3f147
Kintsugi. <erzhuotang@gmail.com>
2026-03-11 10:41:42 +0800

    first commit

---
+++ Source/Cloud_test/Cloud_test.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cloud_test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cloud_test, "Cloud_test" );
